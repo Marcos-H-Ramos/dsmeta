9275130b9286d998c4dde27ca07452b3d9d0a5ad
Marcos Ramos <marcoshenriqueramos@outlook.com>
2022-11-18 21:36:10 -0300

    Twillio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -18,13 +18,13 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 
-	public Page<Sale> findSales(String minDate,String maxDate,Pageable pageable) {
-		
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
-		
+
 		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
-		
+
 		return repository.findSales(min, max, pageable);
 	}
 
